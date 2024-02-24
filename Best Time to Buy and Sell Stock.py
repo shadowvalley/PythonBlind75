fa49a561038a0ea7cd1b9d19ec75c777d677ad04
Satyam Singh <ssatsin2308@gmail.com>
2024-02-24 11:28:19 +0530

    Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    # TC: O(N) SC : O(1)
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0,1
+        max_profit = 0
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                max_profit = max(max_profit, prices[right] - prices[left])
+            else:
+                left = right 
+            right += 1
+        return max_profit
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Arrays ->
 
-1.  Two Sum - https://leetcode.com/problems/two-sum/description/
+1. Two Sum - https://leetcode.com/problems/two-sum/description/
+2. Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
